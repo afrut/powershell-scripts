4cbf2eda3936e299aa441dceecfd7b806857059c
eric373 <25942990+eric373@users.noreply.github.com>
2021-10-29 17:08:52 -0400

    Added basic control structures

--- snippets.ps1
+++ snippets.ps1
@@ -1,14 +1,47 @@
-#----------------------------------------------------------------------
-#
 # This file serves as a reference for some commands in powershell
-#
-#----------------------------------------------------------------------
-# print to console
-Write-Host 'hello world'
 
 # clear screen
 Clear-Host
 
+# print to console
+Write-Host "Hello, World!"
+
+$myvar = "foo"                          # a string variable
+$myint = 100                             # an integer variable
+$array1 = @(1,2,3,4)                    # an integer array with initialization
+$array2 = New-Object string[] 3         # a string array without initialization
+$map1 = @{foo = 0; bar = 1; baz = 2}    # a map with initilization
+$map2 = @{}                             # a map without initialization
+$list = New-Object Collections.Generic.List[Int]   # a list
+
+# A typical for loop
+for($cnt = 0; $cnt -lt $array2.length; $cnt++)
+{
+    $elem = $myvar + [string]$cnt       # create a string
+    $map2.add($cnt, $elem)              # add an element to a map
+    $array2[$cnt] = $elem               # change an element in an array
+}
+
+# A while loop
+$cnt = 0
+while($cnt -lt $myint)
+{
+    $elem = Get-Random -Maximum 999 # get a random integer with a maximum of 999
+    if($elem % 2 -eq 0)             # if the random integer is even add it to the lsit
+    {
+        $list.Add($elem)
+    }
+    $cnt++
+}
+Write-Host "Number of elements added: $($list.Count)"
+
+# recursively search for all files in the current directory with a
+# file depth of not more than 2 and include hidden/system files
+#Get-ChildItem -Path '.\' -Recurse -Depth 2 -Force
+<# TODO:
+select-object
+foreach-object
+#>
 
 
 
@@ -19,9 +52,6 @@ Clear-Host
 # Getting child items in a directory
 #
 #----------------------------------------------------------------------
-# recursively search for all files in the current directory with a
-# file depth of not more than 2 and include hidden/system files
-Get-ChildItem -Path '.\' -Recurse -Depth 2 -Force
 
 # recursively search in a specified directory for files that match
 # a single string pattern and return only their names
@@ -47,8 +77,8 @@ Get-ChildItem -Path 'D:\src\python\' -Recurse -Exclude $ext
 # Using the sort object
 #
 #---------------------------------------------------------------------
-$ret = Get-ChildItem -Path 'D:\src' -Recurse -Exclude $ext
-Write-Host $ret.GetType()
+# $ret = Get-ChildItem -Path 'D:\src' -Recurse -Exclude $ext
+# Write-Host $ret.GetType()
 
 # something different
 
