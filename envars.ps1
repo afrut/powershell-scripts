4699c5f6816c3a67edd771be556e8a3b0d1bf785
eric373 <25942990+eric373@users.noreply.github.com>
2021-10-30 10:41:25 -0400

    Fixed a bug where the machine-scoped PATH was being set to user-scoped PATH

--- envars.ps1
+++ envars.ps1
@@ -7,7 +7,7 @@ $envars = [Environment]::GetEnvironmentVariables().Keys
 $usrvars = New-Object Collections.Generic.List[String]
 $machinevars = New-Object Collections.Generic.List[String]
 $envars | ForEach-Object {
-    $envar = [Environment]::GetEnvironmentVariable($_, "user")
+    $envar = [Environment]::GetEnvironmentVariable($_, [System.EnvironmentVariableTarget]::User)
     if($envar.Length -gt 0) { $usrvars.Add($_) }
     else { $machinevars.Add($_) }
 }
@@ -17,8 +17,8 @@ $usrvars | Out-File -File "./outputs/env-user-vars.txt"
 $machinevars | Out-File -File "./outputs/env-machine-vars.txt"
 
 # Get path variables.
-$usrpath = [Environment]::GetEnvironmentVariable("PATH", "user")
-$machinepath = [Environment]::GetEnvironmentVariable("PATH")
+$usrpath = [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
+$machinepath = [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
 
 # Write to output files.
 $usrpathout = "outputs/env-path-usr.txt"
@@ -26,9 +26,10 @@ $usrpath.Split(";") | Where-Object {$_.Length -gt 0} | Select-Object -Unique |
     Out-File -File $usrpathout
 $machinepathout = "outputs/env-path-machine.txt"
 $machinepath.Split(";") | Where-Object {$_.Length -gt 0} | Select-Object -Unique |
-Out-File -File $machinepathout
+    Out-File -File $machinepathout
 
 # Read in new user path variable.
-$usrpathnew = (Get-Content $usrpathout) -Join ";"
-$machinepathnew = (Get-Content $machinepathout) -Join ";"
-# [Environment]::SetEnvironmentVariable("PATH", $machinepathnew, "machine")
+$usrpathnew = (Get-Content "outputs/env-path-usr-new.txt") -Join ";"
+$machinepathnew = (Get-Content "outputs/env-path-machine-new.txt") -Join ";"
+[Environment]::SetEnvironmentVariable("PATH", $usrpathnew, [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("PATH", $machinepathnew, [System.EnvironmentVariableTarget]::Machine)
