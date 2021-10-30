aa88599fe531fd013f984d5fc1a103ffaa04fe54
eric373 <25942990+eric373@users.noreply.github.com>
2021-10-30 13:25:16 -0400

    Completed installation and configuration

--- env.cmd
+++ env.cmd
@@ -1,2 +1,5 @@
-set PATH=D:\Scala\scala-2.12.15\bin;D:\Scala\sbt\bin;C:\Program Files\Java\jdk1.8.0_202\bin;D:\Program Files\Git\cmd
+set PATH=C:\WINDOWS\system32;D:\Scala\scala-2.12.15\bin;D:\Scala\sbt\bin;C:\Program Files\Java\jdk1.8.0_202\bin;D:\Program Files\Git\cmd;D:\Spark\spark-3.0.3-bin-hadoop2.7\bin;D:\Spark\hadoop-2.7.1\bin;D:\Spark\hadoop-2.7.1\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
+set SPARK_HOME=D:\Spark\spark-3.0.3-bin-hadoop2.7
+set HADOOP_HOME=D:\Spark\hadoop-2.7.1
 cls
