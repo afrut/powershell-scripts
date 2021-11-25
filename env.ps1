9e2b76323f8a694aca5dfd6b99b5cf11177f6d39
eric373 <25942990+eric373@users.noreply.github.com>
2021-11-25 15:47:38 -0500

    Modified to use a powershell script to set environment variables

---
+++ env.ps1
@@ -0,0 +1,4 @@
+$Env:PATH="C:\WINDOWS\system32;D:\Scala\scala-2.12.15\bin;D:\Scala\sbt\bin;C:\Program Files\Java\jdk1.8.0_202\bin;D:\Program Files\Git\cmd;D:\Spark\spark-3.0.3-bin-hadoop2.7\bin;D:\Spark\hadoop-2.7.1\bin"
+$Env:JAVA_HOME="C:\Program Files\Java\jdk1.8.0_202"
+$Env:SPARK_HOME="D:\Spark\spark-3.0.3-bin-hadoop2.7"
+$Env:HADOOP_HOME="D:\Spark\hadoop-2.7.1"
