389faa3fe0bba7a3e5904b492d0496021182f002
eric373 <25942990+eric373@users.noreply.github.com>
2022-01-16 13:25:01 -0500

    Add script to start other processes

---
+++ start-other-processes.ps1
@@ -0,0 +1,19 @@
+# Reference for starting other programs and processes
+
+# Start Visual Studio Code at a certain directory.
+code "D:\src\powershell-scripts\"
+
+# Open an Explorer window at a certain directory.
+explorer "D:\src\"
+
+# Start a powershell session.
+$StartInfo = New-Object System.Diagnostics.ProcessStartInfo
+$StartInfo.FileName = "$pshome\powershell.exe"
+$StartInfo.Arguments = "-NoExit
+ -Command `$Host.UI.RawUI.WindowTitle=`'Test Powershell Session`';
+           Set-Location `'D:\src\`'
+"
+[System.Diagnostics.Process]::Start($StartInfo)
+
+# Run a shortcut.
+Invoke-Item '.\resources\sample-shortcut.lnk'
